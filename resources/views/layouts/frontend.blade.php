9a36af19852e0bdfd2f132bfd1d49ed2b8d891ca
Nibezak <kevin.nibeza@gmail.com>
2024-06-08 13:43:20 +0300

    pushing changes I just made

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,106 +1,115 @@
 <!DOCTYPE html>
-<html lang="zxx"> 
+<html lang="zxx">
+
 <head>
-	@yield('seo')
-	<!-- Favicon -->
-	<link href="/storage/{{$shareSettings->favicon}}" rel="shortcut icon"/>
+    @yield('seo')
+    <!-- Favicon -->
+    @if ($shareSettings->favicon != null)
+        <link href="/storage/{{ $shareSettings->favicon }}" rel="shortcut icon" />
+    @endif
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
 
-	<!-- Google Font -->
-	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
 
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/all.css') }}" />
 
-	<!-- Stylesheets -->
-	<link rel="stylesheet" href="{{ asset('frontend/css/all.css') }}"/>
-
-	<link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}"/>
-	<!-- font-owesome icons link -->
+    <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}" />
+    <!-- font-owesome icons link -->
     <link href="{{ asset('frontend/fontawesome/css/all.css') }}" rel="stylesheet">
 
-	<livewire:styles />
-	@yield('css')
+    <livewire:styles />
+    @yield('css')
 
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	@if($shareSettings->google_analytics != null)
-	<script async src="https://www.googletagmanager.com/gtag/js?id={{ $shareSettings->google_analytics }}"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    @if ($shareSettings->google_analytics != null)
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ $shareSettings->google_analytics }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
 
-	  gtag('config', '{{ $shareSettings->google_analytics }}');
-	</script>
-	@endif
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+
+            gtag('config', '{{ $shareSettings->google_analytics }}');
+        </script>
+    @endif
 
 </head>
+
 <body>
-	
-	<!-- Header section -->
-	<header class="header-section">
-		<div class="header-top">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-2 text-center text-lg-left">
-						<!-- logo -->
-						<a href="/" class="site-logo">
-							<img src="/storage/{{ $shareSettings->logo }}" alt="">
-						</a>
-					</div>
-					<!-- search area -->
-					<livewire:search-dropdown>
-					<div class="col-xl-4 col-lg-5">
-						<div class="user-panel">
-							<div class="up-item">
-								<div class="shopping-card">
-									<i class="flaticon-heart"></i>
-									@if(Cart::instance('wishlist')->count() != 0)
-										<span>{{ Cart::instance('wishlist')->count() }}</span>
-									@endif
-								</div>
-								<a href="{{ route('wishlist.index') }}">Wishlist</a>
-							</div>
-							<div class="up-item">
-								<div class="shopping-card">
-									<i class="flaticon-bag"></i>
-									<span>{{ Cart::instance('default')->count() }}</span>
-								</div>
-								<a href="{{ route('cart.index') }}">Shopping Cart</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<livewire:nav-bar>
-	</header>
-	<!-- Header section end -->
 
-	@yield('content')
+    <!-- Header section -->
+    <header class="header-section">
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 text-center text-lg-left">
+                        <!-- logo -->
+                        @if ($shareSettings != null)
+                            <a href="/" class="site-logo">
+                                <img src="/storage/{{ $shareSettings->logo }}" alt="">
+                            </a>
+                        @endif
+                    </div>
+                    <!-- search area -->
+                    <livewire:search-dropdown>
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="user-panel">
+                                <div class="up-item">
+                                    <div class="shopping-card">
+                                        <i class="flaticon-heart"></i>
+                                        @if (Cart::instance('wishlist')->count() != 0)
+                                            <span>{{ Cart::instance('wishlist')->count() }}</span>
+                                        @endif
+                                    </div>
+                                    <a href="{{ route('wishlist.index') }}">Wishlist</a>
+                                </div>
+                                <div class="up-item">
+                                    <div class="shopping-card">
+                                        <i class="flaticon-bag"></i>
+                                        <span>{{ Cart::instance('default')->count() }}</span>
+                                    </div>
+                                    <a href="{{ route('cart.index') }}">Shopping Cart</a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <livewire:nav-bar>
+    </header>
+    <!-- Header section end -->
 
-
-	<!-- Footer section -->
-	<livewire:footer-detail>
-	<!-- Footer section end -->
+    @yield('content')
 
 
+    <!-- Footer section -->
+    <livewire:footer-detail>
+        <!-- Footer section end -->
 
-	<!--====== Javascripts & Jquery ======-->
-	<livewire:scripts />
-	<script src="{{ asset('frontend/js/all.js') }}"></script>
 
-	<script src="{{ asset('js/toastr.js') }}"></script>
-	<script>
-	    @if(Session::has('success'))
-	    toastr.success("{{ Session::get('success')}}")
-	    @endif
-	</script>
 
-	<script>
-	    @if(Session::has('error'))
-	    toastr.error("{{ Session::get('error')}}")
-	    @endif
-	</script>
+        <!--====== Javascripts & Jquery ======-->
+        <livewire:scripts />
+        <script src="{{ asset('frontend/js/all.js') }}"></script>
 
-	@yield('scripts')
+        <script src="{{ asset('js/toastr.js') }}"></script>
+        <script>
+            @if (Session::has('success'))
+                toastr.success("{{ Session::get('success') }}")
+            @endif
+        </script>
 
-	</body>
+        <script>
+            @if (Session::has('error'))
+                toastr.error("{{ Session::get('error') }}")
+            @endif
+        </script>
+
+        @yield('scripts')
+
+</body>
+
 </html>
