e3e6e5c54a469e9379d589013d664a114acd6aa4
Nibezak <kevin.nibeza@gmail.com>
2024-08-27 14:38:15 +0300

    removing the about section from the site

--- app/Http/Livewire/NavBar.php
+++ app/Http/Livewire/NavBar.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Category;
 use Livewire\Component;
-use App\About;
+// use App\About;
 
 
 class NavBar extends Component
@@ -12,8 +12,8 @@ class NavBar extends Component
     public function render()
     {
     	$navCategories = Category::all();
-         $about = About::firstOrFail();
-
-        return view('livewire.nav-bar', compact('navCategories', 'about'));
+        //  $about = About::firstOrFail();
+// 
+        return view('livewire.nav-bar', compact('navCategories'));
     }
 }
